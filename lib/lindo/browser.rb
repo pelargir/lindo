23dc4ac71e3594eb535a53eea0de2bd6fc3b642f
Josh Sharpe <josh.m.sharpe@gmail.com>
2009-06-30 09:32:14 -0400

    added tests

--- lib/lindo/browser.rb
+++ lib/lindo/browser.rb
@@ -11,7 +11,7 @@ module Lindo
           `gnome-open #{url}`
         elsif kde?
           `kfmclient openURL #{url}`
-        elsif generic_linux?
+        elsif linux?
           `firefox #{url}` #not the default browser.
         else
           raise "Unrecognized OS. Browser can't be found."
@@ -31,16 +31,16 @@ module Lindo
         host.include?('mswin')
       end
       
-      def generic_linux?
-        host.include?('linux')
-      end
-      
       def gnome?
         Kernel.system("which gnome-open")
       end
       
       def kde?
         Kernel.system("which kfmclient")
+      end
+
+      def linux?
+        host.include?('linux')
       end
     end
   end
--- test/lindo/browser_test.rb
+++ test/lindo/browser_test.rb
@@ -8,27 +8,55 @@ require 'lindo'
 class BrowserTest < Test::Unit::TestCase
   
   def z_test_open_on_macos # TODO
+    Browser.expects(:gnome?).returns(false)
+    Browser.expects(:kde?).returns(false)
     Browser.expects(:macos?).returns(true)
     Browser.expects(:system).with("open foo")
     Browser.open("foo")
   end
   
   def z_test_open_on_windows # TODO
+    Browser.expects(:gnome?).returns(false)
+    Browser.expects(:kde?).returns(false)
     Browser.expects(:macos?).returns(false)
     Browser.expects(:windows?).returns(true)
     Browser.expects(:system).with("'C:\Program Files\Internet Explorer\IEXPLORE.EXE' foo")
     Browser.open("foo")
   end
   
-  def z_test_open_on_windows # TODO
+  def z_test_open_on_kde # TODO
+    Browser.expects(:gnome?).returns(false)
+    Browser.expects(:kde?).returns(true)
+    Browser.expects(:macos?).returns(false)
+    Browser.expects(:windows?).returns(false)
+    Browser.expects(:linux?).returns(false)
+    Browser.expects(:system).with("kfmclient openURL foo")
+    Browser.open("foo")
+  end
+
+  def z_test_open_on_gnome # TODO
+    Browser.expects(:gnome?).returns(true)
+    Browser.expects(:kde?).returns(false)
+    Browser.expects(:macos?).returns(false)
+    Browser.expects(:windows?).returns(false)
+    Browser.expects(:linux?).returns(false)
+    Browser.expects(:system).with("gnome-open foo")
+    Browser.open("foo")
+  end
+
+  def z_test_open_on_gnome # TODO
+    Browser.expects(:gnome?).returns(false)
+    Browser.expects(:kde?).returns(false)
     Browser.expects(:macos?).returns(false)
     Browser.expects(:windows?).returns(false)
     Browser.expects(:linux?).returns(true)
-    Browser.expects(:system).with("kfmclient openURL foo")
+    Browser.expects(:system).with("firefox foo")
     Browser.open("foo")
   end
   
   def test_open_with_unrecognized_os
+    Browser.expects(:gnome?).returns(false)
+    Browser.expects(:kde?).returns(false)
     Browser.expects(:macos?).returns(false)
     Browser.expects(:windows?).returns(false)
     Browser.expects(:linux?).returns(false)
